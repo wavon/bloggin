69f396c127145493d268faea1eb3e606cd21876f
Ondine Gallatin <ogallatin@gmail.com>
2017-12-11 13:34:33 -0800

    Solved user authentication error. Changed request.user to lowercase.

--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,15 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
@@ -18,8 +27,8 @@ def post_new(request):
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = request.User
-            post.published_date = timezone.now()
+            post.author = request.user
+            # post.published_date = timezone.now() makes a new post publish immediately
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -33,7 +42,6 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
