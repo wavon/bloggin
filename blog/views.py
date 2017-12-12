4274cb80e831b34aadbec5ab74854144e764fd87
Ondine Gallatin <ogallatin@gmail.com>
2017-12-11 17:04:36 -0800

    Added drafts page. Added publish and delete buttons.

--- blog/views.py
+++ blog/views.py
@@ -28,7 +28,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            # post.published_date = timezone.now() makes a new post publish immediately
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -47,3 +46,8 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
